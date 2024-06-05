29885665c9190e16e48ecba3fc197da576c00ade
Roman-G-579 <romangury579@gmail.com>
2024-06-05 20:39:27 +0300

    updated local db address

--- Phase_B/gptalk-backend/src/config/config.ts
+++ Phase_B/gptalk-backend/src/config/config.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 export class Config {
-	public static DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/gptalk';
+	public static DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/gptalk';
 
 	public static PROTOCOL = process.env.PROTOCOL || 'http';
 
