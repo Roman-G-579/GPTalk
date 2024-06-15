f48f45459bebefa274a105b75e0e8786d904fcf4
Roman-G-579 <romangury579@gmail.com>
2024-06-15 18:49:16 +0300

    removed addUser function from user controller

--- Phase_B/gptalk-backend/src/controllers/user.controller.ts
+++ Phase_B/gptalk-backend/src/controllers/user.controller.ts
@@ -2,21 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { Config } from '../config/config';
 import { UserModel } from '../models/user.interface';
 
-export async function addUserMiddleware(req: Request, res: Response, next: NextFunction) {
-	try {
-		// get the values of the user from the request body
-		const { username, email, age, role } = req.body;
-
-		// save the user in the database
-		const user = await UserModel.create({ username, email, age, role });
-
-		// return the result
-		return res.send({ user });
-	} catch (err) {
-		next(err);
-	}
-}
-
 export async function getUserMiddleware(req: Request, res: Response, next: NextFunction) {
 	try {
 		// get the mongo id from the request body
