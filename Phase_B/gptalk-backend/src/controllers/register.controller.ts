c460dfc6c3469eb26f3eae3dc58b20af393456ec
Roman-G-579 <romangury579@gmail.com>
2024-06-15 18:50:00 +0300

    added register controller and router

---
+++ Phase_B/gptalk-backend/src/controllers/register.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from 'express';
+import { UserModel } from '../models/user.interface';
+import nodemailer from 'nodemailer';
+import bcrypt from 'bcrypt';
+import { Config } from '../config/config';
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS) || 10;
+
+/**
+ * Creates a user with the given details
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function registerMiddleware(req: Request, res: Response, next: NextFunction) {
+	try {
+		const { firstName, lastName, email, password } = req.body;
+
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: Config.ADMIN_EMAIL,
+				pass: Config.ADMIN_PASSWORD,
+			},
+			tls: {
+				rejectUnauthorized: false
+			}
+		});
+
+		let mailOptions = {
+			from: Config.ADMIN_EMAIL,
+			to: email,
+			subject: 'Welcome to GPTalk!',
+			text: `${firstName}, \n your account has been created. Welcome to GPTalk!`
+		};
+
+		transporter.sendMail(mailOptions, function(err, info){
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Email sent: ' + info.response);
+			}
+		});
+
+		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+		await UserModel.create({firstName, lastName, email: email.toLowerCase(), password: hashedPassword});
+		return res.send({response: 'success', email: email.toLowerCase(), firstName, lastName});
+
+	} catch (err) {
+		next(err);
+	}
+}
