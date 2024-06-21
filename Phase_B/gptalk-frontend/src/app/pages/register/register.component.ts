9e157bcfb50881dedd52e3ae5722092e7fb1dff1
Roman-G-579 <romangury579@gmail.com>
2024-06-21 20:00:54 +0300

    Added new design and functionality to registration screen

--- Phase_B/gptalk-frontend/src/app/pages/register/register.component.ts
+++ Phase_B/gptalk-frontend/src/app/pages/register/register.component.ts
@@ -1,19 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { AutoFocus } from 'primeng/autofocus';
+import { StepperModule } from 'primeng/stepper';
+import { NgClass } from '@angular/common';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { PasswordModule } from 'primeng/password';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { Ripple } from 'primeng/ripple';
+import { PrimeNGConfig } from 'primeng/api';
+import { RegisterService } from '../../core/services/register.service';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
+    FloatLabelModule,
+    InputTextModule,
+    InputGroupModule,
+    ButtonModule,
     FormsModule,
-    NgIf,
+    CardModule,
+    AutoFocus,
+    StepperModule,
+    NgClass,
+    IconFieldModule,
+    InputIconModule,
+    PasswordModule,
+    ToggleButtonModule,
+    Ripple,
+    MessageModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   firstName!: string;
   lastName!: string;
   username!: string;
@@ -21,15 +49,33 @@ export class RegisterComponent {
   password!: string;
   confirmPassword!: string;
 
-  printInfo() {
-    console.log(this.firstName)
-    console.log(this.lastName)
-    console.log(this.username)
-    console.log(this.email)
-    console.log(this.password)
+  active: number = 0; //active page number of the registration section
+
+  constructor(private primengConfig: PrimeNGConfig, private registerService: RegisterService) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 
-  submitName() {
+  // calls the register function using the register service using the given details
+  submitDetails() {
+    // takes the registration details from the text input elements
+    const userData = {
+      username: this.username,
+      password: this.password,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email
+    };
 
+    //calls the register service's registration function
+    this.registerService.registerUser(userData).subscribe({
+      next: res => {
+        console.log("registration successful!",res)
+      },
+      error: err => {
+        console.log("registration failed ",err);
+      }
+    })
   }
 }
