439c0259b0b852589e91ce0760b5354ec3171866
Roman-G-579 <romangury579@gmail.com>
2024-06-29 13:53:03 +0300

    Added initial content to home component

--- Phase_B/gptalk-frontend/src/app/pages/home/home.component.ts
+++ Phase_B/gptalk-frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,4 +13,34 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+
+  date = new Date();
+  partOfDay: string = "Morning";
+  firstName = signal("User");
+
+  ngOnInit() {
+    this.setUserFirstName();
+  }
+
+  // Returns the current part of day based on the user's timezone
+  getTimeOfDay() {
+    const hrs = this.date.getHours();
+    if (hrs < 12 ) {
+      return "Morning";
+    }
+    else if (hrs >= 12 && hrs <= 17) {
+      return "Afternoon";
+    }
+    else {
+      return "Evening";
+    }
+  }
+
+  // Returns the currently logged user's first name
+  setUserFirstName() {
+    const fName =  this.authService.userData().user.firstName;
+    this.firstName.set(fName);
+  }
+}
