38746bedb64bb372381884c19d1030e6a7983e6c
Roman-G-579 <romangury579@gmail.com>
2024-07-06 19:37:52 +0300

    more minor changes to exercise interface and enum

--- Phase_B/gptalk-frontend/src/models/enums/exercise-type.enum.ts
+++ Phase_B/gptalk-frontend/src/models/enums/exercise-type.enum.ts
@@ -1,7 +1,7 @@
 export enum ExerciseType {
   FillInTheBlank = 'Fill in the blank',
   TranslateWord = 'Choose the correct translation',
-  WriteTheSentence = 'Write the sentence in ',
+  TranslateTheSentence = 'Translate the sentence',
   CompleteTheConversation = 'Complete the conversation',
   MatchTheWords = 'Match the words'
 }
--- Phase_B/gptalk-frontend/src/models/exercise.interface.ts
+++ Phase_B/gptalk-frontend/src/models/exercise.interface.ts
@@ -9,4 +9,5 @@ export interface Exercise {
   correctWordPairs?: [string, string][]; // Left string - the word in language 1, right string - the equivalent in language 2
   randomizedWordPairs?: [string, string][]; // The word pairs with each column being independently shuffled
   answer?: string;
+  translation?: string;
 }
