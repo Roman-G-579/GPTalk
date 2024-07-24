b880954ef83a4648faec280370394fa68786db16
Roman-G-579 <romangury579@gmail.com>
2024-07-24 19:04:19 +0300

    Updated comments

--- Phase_B/gptalk-frontend/src/app/core/services/lesson-generator.service.ts
+++ Phase_B/gptalk-frontend/src/app/core/services/lesson-generator.service.ts
@@ -15,63 +15,64 @@ export class LessonGeneratorService {
   private readonly apiUrl = environment.apiUrl;
   private readonly http = inject(HttpClient);
 
-  mockExercise_FillInTheBlank: Exercise = {
-    type: ExerciseType.FillInTheBlank,
-    answer: "הטיסה ממריאה מחר בבוקר",
-    choices: ["חתול","קפה","לרוץ"],
-    translation: "The flight takes off tomorrow"
-  }
-
-  mockExercise_TranslateWord: Exercise = {
-    type: ExerciseType.TranslateWord,
-    choices: ["מכונית", "ספר", "שולחן", "תפוח"],
-    translations: ["car", "book", "table", "apple"]
-  }
-
-  mockExercise_TranslateSentence: Exercise = {
-    type: ExerciseType.TranslateTheSentence,
-    question: "קוראים לי דני",
-    answer: "My name is Danny"
-  }
-
-  mockExercise_CompleteTheConversation: Exercise = {
-    type: ExerciseType.CompleteTheConversation,
-    question: "מתי אתה מתכנן לסיים את הפרויקט?",
-    choices: ["אני מתכנן לסיים את הפרויקט בסוף השבוע.", "אני מתכננים לסיים את הפרויקט בסוף השבוע."],
-    translation: "When do you plan to finish the project? / I plan to finish the project by the end of the week."
-  }
-
-  mockExercise_MatchTheWords: Exercise = {
-    type: ExerciseType.MatchTheWords,
-    "correctPairs": [
-      ["מכונית", "car"],
-      ["בית", "house"],
-      ["חתול", "cat"],
-      ["עץ", "tree"],
-      ["ספר", "book"]
-    ]
-  }
-
-  mockExercise_ReorderSentence: Exercise = {
-    type: ExerciseType.ReorderSentence,
-    "answer": "הילד הלך לבית הספר עם חבריו.",
-    "translation": "The boy went to school with his friends."
-  }
-
-  mockExercise_MatchTheCategory: Exercise = {
-    type: ExerciseType.MatchTheCategory,
-    "cat_a": "חיות",
-    "cat_b": "פירות",
-    "words_a": ["כלב", "חתול", "זאב" ,"סוס"],
-    "words_b": ["תפוח", "בננה","ענבים" ,"תפוז"]
-  }
+  // mockExercise_FillInTheBlank: Exercise = {
+  //   type: ExerciseType.FillInTheBlank,
+  //   answer: "הטיסה ממריאה מחר בבוקר",
+  //   choices: ["חתול","קפה","לרוץ"],
+  //   translation: "The flight takes off tomorrow"
+  // }
+  //
+  // mockExercise_TranslateWord: Exercise = {
+  //   type: ExerciseType.TranslateWord,
+  //   choices: ["מכונית", "ספר", "שולחן", "תפוח"],
+  //   translations: ["car", "book", "table", "apple"]
+  // }
+  //
+  // mockExercise_TranslateSentence: Exercise = {
+  //   type: ExerciseType.TranslateTheSentence,
+  //   question: "קוראים לי דני",
+  //   answer: "My name is Danny"
+  // }
+  //
+  // mockExercise_CompleteTheConversation: Exercise = {
+  //   type: ExerciseType.CompleteTheConversation,
+  //   question: "מתי אתה מתכנן לסיים את הפרויקט?",
+  //   choices: ["אני מתכנן לסיים את הפרויקט בסוף השבוע.", "אני מתכננים לסיים את הפרויקט בסוף השבוע."],
+  //   translation: "When do you plan to finish the project? / I plan to finish the project by the end of the week."
+  // }
+  //
+  // mockExercise_MatchTheWords: Exercise = {
+  //   type: ExerciseType.MatchTheWords,
+  //   "correctPairs": [
+  //     ["מכונית", "car"],
+  //     ["בית", "house"],
+  //     ["חתול", "cat"],
+  //     ["עץ", "tree"],
+  //     ["ספר", "book"]
+  //   ]
+  // }
+  //
+  // mockExercise_ReorderSentence: Exercise = {
+  //   type: ExerciseType.ReorderSentence,
+  //   "answer": "הילד הלך לבית הספר עם חבריו.",
+  //   "translation": "The boy went to school with his friends."
+  // }
+  //
+  // mockExercise_MatchTheCategory: Exercise = {
+  //   type: ExerciseType.MatchTheCategory,
+  //   "cat_a": "חיות",
+  //   "cat_b": "פירות",
+  //   "words_a": ["כלב", "חתול", "זאב" ,"סוס"],
+  //   "words_b": ["תפוח", "בננה","ענבים" ,"תפוז"]
+  // }
 
   /**
-   * Calls a random exercise generator function
+   * Generates the specified amount of random exercises by calling OpenAI's API
    *
    * @param language the language of the generated exercise
    * @param difficulty difficulty the difficulty level
    * @param amount the amount of exercises to be generated
+   * @returns the exercises observable
    */
   generateLesson(language: Language, difficulty: Difficulty, amount: number): Observable<Exercise[]> {
     let keyWords = genUtil.insertKeyWords(difficulty); // Insert special keywords for the api based on the difficulty
@@ -120,12 +121,12 @@ export class LessonGeneratorService {
   }
 
   /**
-   * Sends a query to the API to generate a "fill in the blanks" exercise, using the given parameters
+   * Creates a query for the API to generate a "fill in the blanks" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateFillInTheBlank(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  generateFillInTheBlank(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     // Exercise-specific parameters
     let numOfAnswers: number;
 
@@ -142,16 +143,16 @@ export class LessonGeneratorService {
       numOfAnswers = 5;
     }
 
-    return `generate an object in ${language}, ${difficulty} difficulty. 'answer' is the sentence, 'translation' is its english translation, 'choices' is 3 random words, not found in "answer". Focus on topics: ${keyWords[0]}. {"answer": "", "choices:" [], "translation": ""}`;
+    return `generate an object in ${language}, ${difficulty} difficulty. 'answer' is the sentence, 'translation' is its english translation, 'choices' is ${numOfAnswers} random words, not found in "answer". Focus on topics: ${keyWords[0]}. {"answer": "", "choices:" [], "translation": ""}`;
   }
 
   /**
-   * Sends a query to the API to generate a "translate words" exercise, using the given parameters
+   * Creates a query for the API to generate a "translate words" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateTranslateWord(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  generateTranslateWord(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     // Exercise-specific parameters
     let numOfAnswers: number;
 
@@ -172,32 +173,32 @@ export class LessonGeneratorService {
   }
 
   /**
-   * Sends a query to the API to generate a "write the sentence in the specified language" exercise, using the given parameters
+   * Creates a query for the API to generate a "write the sentence in the specified language" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateTranslateTheSentence(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  generateTranslateTheSentence(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     return `generate a "translate the sentence" exercise, difficulty: ${difficulty}. Focus on topics: ${keyWords[0]}. {"question": "english_sentence", "answer": "${language}_translation"}`;
   }
 
   /**
-   * Sends a query to the API to generate a "complete the conversation" exercise, using the given parameters
+   * Creates a query for the API to generate a "complete the conversation" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateCompleteTheConversation(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  generateCompleteTheConversation(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     return `generate a "complete the conversation" exercise, difficulty: ${difficulty}, language: ${language}. First choice is grammatically correct and makes sense, second choice doesn't. Focus on topics: ${keyWords[0]}. { "question": "question/statement", "choices": ["reply1","reply2"] "translation": "english_translation_of_question_&_correct_reply" }`;
   }
 
   /**
-   * Sends a query to the API to generate a "match the words to their translations" exercise, using the given parameters
+   * Creates a query for the API to generate a "match the words to their translations" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateMatchTheWords(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  generateMatchTheWords(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     // Exercise-specific parameters
     let numOfPairs: number;
 
@@ -218,35 +219,46 @@ export class LessonGeneratorService {
   }
 
   /**
-   * Sends a query to the API to generate a "reorder the words into a proper sentence" exercise, using the given parameters
+   * Creates a query for the API to generate a "reorder the words into a proper sentence" exercise, using the given parameters
    * @param language the language of the generated exercise
    * @param difficulty the exercise's difficulty level
    * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
    */
-  generateReorderSentence(language: Language, difficulty: Difficulty, keyWords: string[]) {
-    let sentenceLength: number;
+  generateReorderSentence(language: Language, difficulty: Difficulty, keyWords: string[]): string {
+    let sentenceLength: string;
 
     // Parameters for very easy and easy difficulties
     if (difficulty >= 0 && difficulty < 2) {
-      sentenceLength = 4;
+      sentenceLength = "short";
     }
     // Parameters for medium and hard difficulties
     else if (difficulty >= 3 && difficulty < 4) {
-      sentenceLength = 5;
+      sentenceLength = "medium";
     }
     // Parameters for very hard and expert difficulties
     else {
-      sentenceLength = 6;
+      sentenceLength = "long";
     }
 
-    return `generate a sentence, in ${language}, ${difficulty} difficulty. Focus on topics: ${keyWords[0]} { "answer": "", "translation": "" }`;
+    return `generate a sentence, in ${language}, ${difficulty} difficulty, ${sentenceLength} length. Focus on topics: ${keyWords[0]} { "answer": "", "translation": "" }`;
   }
 
-  generateMatchTheCategory(language: Language, difficulty: Difficulty, keyWords: string[]) {
+  /**
+   * Creates a query for the API to generate a "match the words to their categories" exercise, using the given parameters
+   * @param language the language of the generated exercise
+   * @param difficulty the exercise's difficulty level
+   * @param keyWords a string array of keywords that are sent to the API to narrow the generated results
+   */
+  generateMatchTheCategory(language: Language, difficulty: Difficulty, keyWords: string[]): string {
     return `Generate 2 distinct categories and 4 words for each, in ${language}, ${difficulty} difficulty. Focus on topics: ${keyWords[0]} Ensure the response is strictly in the following JSON format: { "cat_a": "", "cat_b": "", "words_a": [], "words_b": [] }. Do not include any additional keys.`;
   }
 
-  getExerciseFromApi(promptString: string) {
+  /**
+   * Sends a request to the backend to call the API using the given prompt string
+   * @param promptString the prompt string of the exercise to be generated
+   * @returns an observable of the API's response JSON
+   */
+  getExerciseFromApi(promptString: string): Observable<object> {
     const {href} = new URL('generateLesson', this.apiUrl);
 
     // API CONNECTION
