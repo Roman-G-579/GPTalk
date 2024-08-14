7de21ae093c474aaa65e82c78359183432cee510
Roman-G-579 <romangury579@gmail.com>
2024-08-14 12:34:27 +0300

    Updated progress calculation in exp bar

--- Phase_B/gptalk-frontend/src/app/pages/learn/exp-bar/exp-bar.component.ts
+++ Phase_B/gptalk-frontend/src/app/pages/learn/exp-bar/exp-bar.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, inject, signal } from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { PrimeTemplate } from 'primeng/api';
 import { NgClass } from '@angular/common';
 import { LearnService } from '../../../core/services/learn.service';
 import { AuthService } from '../../../core/services/auth.service';
+import {MiscUtils as util} from '../../../core/utils/misc-utils';
 
 @Component({
   selector: 'app-exp-bar',
@@ -27,10 +28,20 @@ export class ExpBarComponent {
   totalExp = this.authService.totalExp;
   level = this.authService.level;
 
-  progressVal = computed(() => this.totalExp() / this.calculateExpForNextLevel() * 100);
+  progressVal = computed(() => {
+    const totalExp = this.totalExp();
+    const expForNextLevel = util.calculateExpForNextLevel(this.level());
+    const expAtLevelStart = expForNextLevel / 2;
 
+    return (totalExp - expAtLevelStart) / (expForNextLevel - expAtLevelStart) * 100;
+  });
+
+  /**
+   * Calculates the required amount of experience points
+   * needed for the next level, based on the user's current level
+   */
   calculateExpForNextLevel(){
-    return (100 * Math.pow(2, this.level()));
+      return (100 * Math.pow(2, this.level()-1));
   }
 
 }
