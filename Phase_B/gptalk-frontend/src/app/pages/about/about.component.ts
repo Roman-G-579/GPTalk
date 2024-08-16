38f059f9c61089d01ef236a934ff6b3ca3276148
Roman-G-579 <romangury579@gmail.com>
2024-08-16 17:04:40 +0300

    Added about page content Changed GPTalk_Logo3's color

--- Phase_B/gptalk-frontend/src/app/pages/about/about.component.ts
+++ Phase_B/gptalk-frontend/src/app/pages/about/about.component.ts
@@ -1,14 +1,45 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     CommonModule,
+    CarouselModule,
+    TagModule,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent { }
+export class AboutComponent {
+  software = [
+    {
+      name: 'Angular',
+      image: 'assets/software-logos/Angular.png',
+    },
+    {
+      name: 'MongoDB',
+      image: 'assets/software-logos/MongoDB.png',
+    },
+    {
+      name: 'Express.js',
+      image: 'assets/software-logos/Express.png',
+    },
+    {
+      name: 'OpenAI',
+      image: 'assets/software-logos/OpenAI.png',
+    },
+    {
+      name: 'Vercel',
+      image: 'assets/software-logos/Vercel.png',
+    }
+  ]
+
+
+
+
+}
