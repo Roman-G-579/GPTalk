1c8a1b4497424af01f1ba7e895ffd56145b87fad
Roman-G-579 <romangury579@gmail.com>
2024-09-11 14:17:11 +0300

    Added filtering by language to leaderboard component

--- Phase_B/gptalk-frontend/src/app/pages/leaderboard/leaderboard.component.ts
+++ Phase_B/gptalk-frontend/src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,18 +1,22 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AnimationOptions, LottieComponent } from 'ngx-lottie';
-import { ToastrService } from 'ngx-toastr';
-import { LeaderboardService } from './leaderboard.service';
-import { MiscUtils } from '../../../app/core/utils/misc.utils';
-import { LeadboardRow } from './leaderboard.interface';
-import { UserAvatarComponent } from '../../../app/core/common/user-avatar/user-avatar.component';
-import { SinceDatePipe } from '../../../app/core/pipes/since-date.pipe';
-import { LoadingComponent } from 'src/app/core/common/loading/loading.component';
+import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AnimationOptions, LottieComponent} from 'ngx-lottie';
+import {ToastrService} from 'ngx-toastr';
+import {LeaderboardService} from './leaderboard.service';
+import {MiscUtils} from '../../../app/core/utils/misc.utils';
+import {LeadboardRow} from './leaderboard.interface';
+import {UserAvatarComponent} from '../../../app/core/common/user-avatar/user-avatar.component';
+import {SinceDatePipe} from '../../../app/core/pipes/since-date.pipe';
+import {LoadingComponent} from 'src/app/core/common/loading/loading.component';
+import {Language} from "../../core/enums/language.enum";
+import {SplitButtonModule} from "primeng/splitbutton";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
 
 @Component({
 	selector: 'app-leaderboard',
 	standalone: true,
-	imports: [CommonModule, LottieComponent, UserAvatarComponent, SinceDatePipe, LoadingComponent],
+  imports: [CommonModule, LottieComponent, UserAvatarComponent, SinceDatePipe, LoadingComponent, SplitButtonModule, DropdownModule, FormsModule],
 	templateUrl: './leaderboard.component.html',
 	styleUrl: './leaderboard.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -31,13 +35,59 @@ export class LeaderboardComponent implements OnInit {
 		{ path: '/assets/lottie/bronze-medal.json' },
 	];
 
+  languageIconRecord: Record<string, string> = {
+    English: 'assets/profile/languages/english.png',
+    Spanish: 'assets/profile/languages/spanish.png',
+    Russian: 'assets/profile/languages/russian.png',
+    Hebrew: 'assets/profile/languages/hebrew.png',
+    NOT_SELECTED: 'assets/profile/languages/global.png',
+  };
+
+  languages = [
+    {
+      name: Language.English,
+      icon: this.languageIconRecord[Language.English],
+    },
+    {
+      name: Language.Spanish,
+      icon: this.languageIconRecord[Language.Spanish],
+    },
+    {
+      name: Language.Russian,
+      icon: this.languageIconRecord[Language.Russian],
+    },
+    {
+      name: Language.Hebrew,
+      icon: this.languageIconRecord[Language.Hebrew],
+    },
+    {
+      name: 'Global',
+      icon: this.languageIconRecord[Language.NOT_SELECTED],
+    }
+  ];
+
+  selectedLanguage = this.languages[4];
+
 	ngOnInit() {
-		this.leaderboardService.getLeaderboard().subscribe({
-			next: (res: { top3Users: LeadboardRow[]; top4To10Users: LeadboardRow[] }) => {
-				this.top3Users.set(res.top3Users);
-				this.top4To10Users.set(res.top4To10Users);
-			},
-			error: () => this.toastr.error('Error occured!', 'Error!'),
-		});
+    this.getLeaderboardByLanguage(Language.NOT_SELECTED);
 	}
+
+  /**
+   * Gets a leaderboard object of the top 10 users, filtered by the given language
+   * If a 'global' language is given, the overall top 10 users will be retrieved (without a language filter)
+   * @param language the filtered language
+   */
+  getLeaderboardByLanguage(language: Language | string) {
+    if (language == 'Global') { language = Language.NOT_SELECTED; }
+
+    this.leaderboardService.getLeaderboard(language as Language).subscribe({
+      next: (res: { top3Users: LeadboardRow[]; top4To10Users: LeadboardRow[] }) => {
+        this.top3Users.set(res.top3Users);
+        this.top4To10Users.set(res.top4To10Users);
+      },
+      error: () => this.toastr.error('Error occurred!', 'Error!'),
+    });
+  }
+
+  protected readonly Language = Language;
 }
